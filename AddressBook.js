80e29ccb97640494d412dad0992a451f298ce669
poojakhandale <poojakhandale2000@gmail.com>
2023-03-31 22:15:23 +0530

    New AddressBook

---
+++ AddressBook.js
@@ -0,0 +1,40 @@
+class Contact {
+  constructor(firstName, lastName, city, phoneNumber) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.city = city;
+    this.phoneNumber = phoneNumber;
+  }
+}
+
+class AddressBook {
+  constructor() {
+    this.contacts = [];
+  }
+
+  addContact(contact) {
+    this.contacts.push(contact);
+  }
+
+  removeContact(index) {
+    this.contacts.splice(index, 1);
+  }
+
+  getContact(index) {
+    return this.contacts[index];
+  }
+
+  getAllContacts() {
+    return this.contacts;
+  }
+}
+
+// example usage
+const john = new Contact("Pooja", "Khandale", "New York", "8369315555");
+const jane = new Contact("rani", "Stark", "Los Angeles", "45454224945");
+const book = new AddressBook();
+
+book.addContact(john);
+book.addContact(jane);
+
+console.log(book.getAllContacts());
